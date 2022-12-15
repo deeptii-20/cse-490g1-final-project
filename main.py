10363dbf355ab57acad68e983bc1c7ab9e436670
Simran Malhi <simran_malhi@outlook.com>
2022-12-14 18:26:11 -0800

    Added plots

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import process_data as pd
 import network as n
 import torch.optim as optim
 import numpy as np
+import matplotlib.pyplot as plt
 
 FILE_NAME = 'data/spotify_dataset.csv'
 
@@ -14,11 +15,11 @@ print("done")
 print()
 
 print("making model...")
-TRAIN_BATCH_SIZE = 50
+TRAIN_BATCH_SIZE = 512
 FEATURE_SIZE = 9
-TEST_BATCH_SIZE = 50
-EPOCHS = 50
-LEARNING_RATE = 0.001
+TEST_BATCH_SIZE = 512
+EPOCHS = 20
+LEARNING_RATE = 0.1
 WEIGHT_DECAY = 0.0005
 PRINT_INTERVAL = 10
 m = n.SpotifyNet(train_data.user_size(), train_data.track_size(), FEATURE_SIZE)
@@ -29,16 +30,39 @@ print("training...")
 optimizer = optim.Adam(m.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=TRAIN_BATCH_SIZE, shuffle=False)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=TEST_BATCH_SIZE, shuffle=False)
+test_losses, test_accuracies, train_losses = [], [], []
 try:
     for epoch in range(1, EPOCHS + 1):
         lr = LEARNING_RATE * np.power(0.25, (int(epoch / 6)))
         train_loss = n.train(m, optimizer, train_loader, epoch, PRINT_INTERVAL)
         test_loss, test_accuracy = n.test(m, test_loader)
+        test_losses.append((epoch, test_loss))
+        test_accuracies.append((epoch, test_accuracy))
+        train_losses.append((epoch, train_loss))
 except KeyboardInterrupt as ke:
         print('Interrupted')
 except:
     import traceback
     traceback.print_exc()
+finally:
+    ep, val = zip(*train_losses)
+    plt.plot(val)
+    plt.xlabel('Epoch')
+    plt.ylabel('Train Loss')
+    plt.title('Train Loss vs Epoch')
+    plt.show()
+    ep, val = zip(*test_losses)
+    plt.plot(val)
+    plt.xlabel('Epoch')
+    plt.ylabel('Test Loss')
+    plt.title('Test Loss vs Epoch')
+    plt.show()
+    ep, val = zip(*test_accuracies)
+    plt.plot(val)
+    plt.xlabel('Epoch')
+    plt.ylabel('Test Accuracy')
+    plt.title('Test Accuracies vs Epoch')
+    plt.show()
 print("done")
 print()
 
